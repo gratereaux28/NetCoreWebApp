70e0960da8ff05aa8478c5c6a5700bf144185b28
gratereaux28 <gratereaux28@hotmail.com>
2023-02-02 14:10:40 -0400

    Creando DTO para las entidades de custumer y user

---
+++ Core/DTOs/UserDTo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+#nullable disable
+
+namespace Core.DTOs
+{
+    public partial class UserDTo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
